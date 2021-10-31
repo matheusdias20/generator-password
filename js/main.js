aeecf1c0488e0a3b59ec507a9129744646f19f1d
Matheus Vieira <mvdias20@gmail.com>
2021-10-31 19:19:04 -0300

    Fix navbar toggle

--- js/main.js
+++ js/main.js
@@ -79,3 +79,13 @@ function getRandomSymbol() {
 }
 
 
+const navSlide = () => {
+	const burguer = document.querySelector('.burguer')
+	const nav = document.querySelector('.nav-links')
+
+	burguer.addEventListener('click', () => {
+		nav.classList.toggle('nav-active')
+	})
+}
+
+navSlide();
