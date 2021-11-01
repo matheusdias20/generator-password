ecf389fada19fa96d52ae60f2e2d355193106272
Matheus Vieira <mvdias20@gmail.com>
2021-10-31 23:48:31 -0300

    1. Try fix bug

--- js/main.js
+++ js/main.js
@@ -82,9 +82,14 @@ function getRandomSymbol() {
 const navSlide = () => {
 	const burguer = document.querySelector('.burguer')
 	const nav = document.querySelector('.nav-links')
+	const navLinks = document.querySelectorAll('.nav-links li')
 
 	burguer.addEventListener('click', () => {
 		nav.classList.toggle('nav-active')
+	})
+
+	navLinks.forEach((link, index) => {
+		link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 1.2}s`
 	})
 }
 
